e6c2fc82c7ca629a914c9a9a08a6d772efdc3a30
uvayankee <ajmarshall2k@gmail.com>
2016-02-07 22:02:15 -0500

    Fixed Error output, added application distribution to gradle

--- src/main/java/marshall/backup/messenger/BackupRunner.java
+++ src/main/java/marshall/backup/messenger/BackupRunner.java
@@ -10,7 +10,7 @@ public class BackupRunner {
     public static void main(String[] args) {
 
         if(args.length != 4) {
-            System.err.println("Params: username, password, messageIdQueryParam");
+            System.err.println("Params: username, password, messageIdQueryParam, outputDirectory");
         } else {
             FacebookClient fbc = new FacebookClient(args[0], args[1], args[2], args[3]);
             fbc.start();
